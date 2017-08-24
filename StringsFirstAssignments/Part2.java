32956412635c76d040186fc2db78d1ff4c0a5260
limon_148 <mehadihasan.cseru@gmail.com>
2017-08-24 12:09:19 -0700

    now class part 2 is modified using startCodon and stopCodon and successfully runs

--- StringsFirstAssignments/Part2.java
+++ StringsFirstAssignments/Part2.java
@@ -1,31 +1,33 @@
 
 public class Part2
 {
-    public String findSimpleGene(String dna){
-        int startIndex = dna.indexOf("ATG");
-        int stopIndex = dna.indexOf("TAA", startIndex + 3);
+    public String findSimpleGene(String dna, String startCodon, String stopCodon){
+        int startIndex = dna.indexOf(startCodon);
+        int stopIndex = dna.indexOf(stopCodon, startIndex + 3);
         while(stopIndex != -1){
             if((stopIndex - startIndex) % 3 == 0){
                 return dna.substring(startIndex, stopIndex + 3);
             }
             else{
-                stopIndex = dna.indexOf("TAA", stopIndex + 1);
+                stopIndex = dna.indexOf(stopCodon, stopIndex + 1);
             }
         }
         return "";
     }
     void testSimpleGene(){
         String dna = "AATGCGTAATTAATCG";
+        String startCodon = "ATG";
+        String stopCodon = "TAA";
         System.out.println("DNA strand is " + dna);
-        String gene = findSimpleGene(dna);
+        String gene = findSimpleGene(dna, startCodon, stopCodon);
         System.out.println("Gene is " + gene);
         dna = "CGATGGTTGATAAGCCTAAGCTATAA";
         System.out.println("DNA strand is " + dna);
-        gene = findSimpleGene(dna);
+        gene = findSimpleGene(dna, startCodon, stopCodon);
         System.out.println("Gene is " + gene);
         dna = "CGATGGTTGATAAGCCTAAGCTAAA";
         System.out.println("DNA strand is " + dna);
-        gene = findSimpleGene(dna);
+        gene = findSimpleGene(dna, startCodon, stopCodon);
         System.out.println("Gene is " + gene);
     }
 }
