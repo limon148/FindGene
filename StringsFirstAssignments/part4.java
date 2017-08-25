9e936f1d8c31e3f2e9bbb3d20339a88f367f04d9
limon_148 <mehadihasan.cseru@gmail.com>
2017-08-25 13:45:10 -0700

    This will read word by word of that URL.

--- StringsFirstAssignments/part4.java
+++ StringsFirstAssignments/part4.java
@@ -1,16 +1,11 @@
 
 import edu.duke.*;
-public class part4
+public class Part4
 {
     public void checkYoytube(){
         URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html"); 
         for(String word : res.words()){
-            int x = word.indexOf("youtube.com");
-            int l = word.length();
-            if(x != -1){
-                int startIndex = word.indexOf("\"");
-                int endIndex = word.lastIndexOf("\"", l);
-            }
+            
         }
     }
 }
