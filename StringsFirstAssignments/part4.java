2524b6381eb7079b9feeeea376e6f582278952a4
limon_148 <mehadihasan.cseru@gmail.com>
2017-08-25 13:12:16 -0700

    this code can read every word from a urlresource.

---
+++ StringsFirstAssignments/part4.java
@@ -0,0 +1,12 @@
+
+import edu.duke.*;
+public class part4
+{
+    public void checkYoytube(){
+        URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html"); 
+        for(String word : res.words()){
+            
+            
+        }
+    }
+}
