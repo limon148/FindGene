09d7735809aaad4f0b505bf6ee631f5795ea7554
limon_148 <mehadihasan.cseru@gmail.com>
2017-08-25 13:48:28 -0700

    This will use lastIndexOf method to find the Youtube link

--- StringsFirstAssignments/part4.java
+++ StringsFirstAssignments/part4.java
@@ -6,9 +6,10 @@ public class Part4
         URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html"); 
         for(String word : res.words()){
             int x = word.indexOf("youtube.com");
+            int l = word.length();
             if(x != -1){
                 int startIndex = word.indexOf("\"");
-                int endIndex = word.indexOf("\"", startIndex + 1);
+                int endIndex = word.lastIndexOf("\"", l);
             }
         }
     }
